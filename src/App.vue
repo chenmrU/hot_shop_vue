6c23c12b8e6ac8f77e3f4f92ecc9644f6f665588
chenmengrui <chenmengrui@kp99.cn>
2020-08-19 14:50:08 +0800

    添加过场动画

--- src/App.vue
+++ src/App.vue
@@ -40,4 +40,55 @@
 
 <style lang="less">
   @import './common/style/mixin';
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+
+  #nav {
+    position: absolute;
+    top: 0;
+    left: 50%;
+    z-index: 1000;
+  }
+
+  .router-view{
+    width: 100%;
+    height: auto;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    margin: 0 auto;
+    -webkit-overflow-scrolling: touch;
+  }
+
+  .slide-right-enter-active,
+  .slide-right-leave-active,
+  .slide-left-enter-active,
+  .slide-left-leave-active{
+    height: 100%;
+    will-change: transform;
+    transition: all 500ms;
+    position: absolute;
+    backface-visibility: hidden;
+  }
+  .slide-right-enter{
+    opacity: 0;
+    transform: translate3d(-100%, 0, 0);
+  }
+  .slide-right-leave-active{
+    opacity: 0;
+    transform: translate3d(100%, 0, 0);
+  }
+  .slide-left-enter{
+    opacity: 0;
+    transform: translate3d(100%, 0, 0);
+  }
+  .slide-left-leave-active{
+    opacity: 0;
+    transform: translate3d(-100%, 0, 0);
+  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/home/Home.vue'
-import Category from "../views/category/Category";
 
 Vue.use(VueRouter)
 
@@ -9,12 +8,18 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      index: 1 // 添加 meta 属性，约定 1 为第一级
+    }
   },
   {
     path: '/category',
     name: 'Category',
-    component: Category,
+    component: () => import(/* webpackChunkName: "about" */ '../views/category/Category.vue'),
+    meta: {
+      index: 2 // 添加 meta 属性，约定 2 为第二级
+    }
   }
 ]
 
