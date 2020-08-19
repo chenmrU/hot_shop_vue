85eefc7287fee46a653fd8bd0aca764939d138b6
chenmengrui <chenmengrui@kp99.cn>
2020-08-19 14:07:42 +0800

    first commit

---
+++ src/views/home/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="home" class="home">
+    hhh
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home'
+  }
+</script>
+
+<style lang="less">
+  .home div {
+    // background: darkblue;
+    width: 75px;
+    height: 75px;
+    font-size: 18PX;
+    i {
+      color: red;
+    }
+  }
+</style>
